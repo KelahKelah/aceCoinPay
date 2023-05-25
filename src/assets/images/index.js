04976fd6b69eac24cc7ddc5f01e07d8a5e12df50
kelechi <kelechiekebor@gmail.com>
2023-05-25 06:42:22 +0100

    added images to index.js

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import cardImg from "./cardImg.png";
+import checkIcon from "./checkIcon.png";
+import masterCardIcon from "./masterCardIcon.png";
+import menuIcon from "./menuIcon.png";
+import logo from "./logo.png";
+
+export { cardImg, checkIcon, masterCardIcon, menuIcon, logo };
